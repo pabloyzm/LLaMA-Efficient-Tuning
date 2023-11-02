7955fc56fd4b5b27767a9e3927983e512efeb1fe
Pablo Ignacio Yáñez Melo <pabloyzm@gmail.com>
2023-11-02 16:34:55 -0300

    Update make_dataset.py

--- src/make_dataset.py
+++ src/make_dataset.py
@@ -141,7 +141,7 @@ def generate_prediction(sample_data, model_script='src/train_bash.py', dataset_p
         trainer.save_metrics("predict", predict_results.metrics)
         result = trainer.get_predictions(predict_results)
 
-    return json.loads(result)["summary"]
+    return result
 
 print("Loading dataset... \n")
 # load dataset avoiding timeout and print progress
